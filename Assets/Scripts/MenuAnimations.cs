1164f92e5235ec8b4f3bdad86e47918ab1fbfaab
marinin97 <uxmarinin@gmail.com>
2025-09-06 10:34:05 +0300

    fix out title text tween

--- Assets/Scripts/MenuAnimations.cs
+++ Assets/Scripts/MenuAnimations.cs
@@ -4,31 +4,28 @@ using TMPro;
 
 public class MenuAnimations : MonoBehaviour
 {
-    public TMP_Text TitleText;
-
-    private RectTransform _titleTextTransform;
+    public RectTransform TitleText;
 
     public void Awake()
     {
-        _titleTextTransform = TitleText.GetComponent<RectTransform>();
-        //ScaleTitleText();
+        ScaleTitleText();
         DropDownTitleText();
     }
 
     private void DropDownTitleText()
     {
-        Vector3 startPosition = new Vector2(0, -239f);
-        _titleTextTransform.position = new Vector2(0, 200);
-        _titleTextTransform.DOAnchorPos(startPosition, 2f);
+        Vector3 startPosition = TitleText.anchoredPosition; 
+        TitleText.anchoredPosition = new Vector2(0, 200);
+        TitleText.DOAnchorPos(startPosition, 2f).SetEase(Ease.OutBounce);
     }
 
     private void ScaleTitleText()
     {
-        TitleText.transform.DOScale(0.8f, 1f).SetLoops(-1, LoopType.Yoyo);
+        TitleText.DOScale(0.8f, 1f).SetLoops(-1, LoopType.Yoyo);
     }
 
     public void OnDestroy()
     {
-        TitleText.transform.DOKill();
+        TitleText.DOKill();
     }
 }
